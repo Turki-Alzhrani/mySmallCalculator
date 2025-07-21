dc11abddfe73a4119e93566b147e506c485a0b91
Turki-Alzhrani <turkix572@gmail.com>
2025-07-21 04:29:04 +0300

    Add README

---
+++ Calculator.js
@@ -0,0 +1,36 @@
+
+let mydis = document.getElementById("display");
+
+function appendToDisplay(input){
+    if (mydis.value === "0") {
+        mydis.value = input;
+    } else {
+        mydis.value += input;
+    }
+}
+
+function clearDisply(){
+    mydis.value = 0 ;
+}
+
+function calculate(){
+    try{
+    mydis.value= eval(mydis.value)
+    }
+    catch(error){
+        mydis.value="Hello World";
+    }
+}
+
+
+function backSpace() {
+    if (mydis.value === "Hello World") {
+        mydis.value = "0";
+    } else {
+        mydis.value = mydis.value.slice(0, -1);
+        if (mydis.value === "") {
+            mydis.value = "0";
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# My Small Calculator
+
+I have completed my first project using HTML, CSS, and JavaScript.  
+I feel proud of myself, and I followed a tutorial on YouTube by [Bro Code](https://www.youtube.com/@BroCodez),  
+but I also made some small changes to make it my own.
+
